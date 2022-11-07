04fd721c738971ce09a3e560023a6d6a33753c11
judycpc <abc0952753111@gmail.com>
2022-11-07 14:57:09 +0800

    add hotelDetail to route & create HotelDetail folder

---
+++ frontend/src/pages/HotelDetail/Components.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+
+export function SentimentRatioCard() {
+    return (
+        <h1>SentimentRatioCard</h1>
+    )
+}
--- frontend/src/pages/HotelDetail/HotelDetail.tsx
+++ frontend/src/pages/HotelDetail/HotelDetail.tsx
@@ -1,7 +1,8 @@
 import { Toolbar } from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
-import { Background, CenterDiv } from "../components/Pages";
-import TitleBar from "../components/TitleBar";
+import { Background, CenterDiv } from "../../components/Pages";
+import TitleBar from "../../components/TitleBar";
+import { SentimentRatioCard } from "./Components";
 
 export default function HotelDetail() {
   useEffect(() => {}, []);
@@ -11,7 +12,8 @@ export default function HotelDetail() {
       <Background>
         <TitleBar title="HotelDetail"></TitleBar>
         <CenterDiv>
-          <h1>HotelDetail</h1>
+          {/* <h1>HotelDetail</h1> */}
+          <SentimentRatioCard />
         </CenterDiv>
       </Background>
     </>
