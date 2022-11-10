12bf6eb9848dee8a64c6317d8e2c82a2ad0ea64c
sylvey <a.few.star@gmail.com>
2022-11-10 23:32:21 +0800

    前端改

--- frontend/src/pages/Home/Components.tsx
+++ frontend/src/pages/Home/Components.tsx
@@ -37,31 +37,31 @@ export function HotelCard(props?: HotelCardProps) {
   return (
     <Box
       style={{
-        height: 321.6,
-        width: 246,
+        height: 360,
+        width: 280,
         background: "#FFFFFF",
         marginTop: 12,
       }}
     >
       <img
         src={require("../../assets/temp/hotelProfile.png")}
-        style={{ height: 146.204, width: 246 }}
+        style={{ height: 176.204, width: 280 }}
       ></img>
-      <div style={{ margin: 5 }}>
+      <div style={{ width: 240, margin: "auto" }}>
         <Title onClick={onClick}>{props?.item?.Name}</Title>
-        <Row>
+        <Row style={{ marginTop: 10 }}>
           <Icon src={require("../../assets/location.png")} />
           <Text>
             {props?.item?.District}, {props?.item?.County}
           </Text>
         </Row>
-        <Row>
+        <Row style={{ marginTop: 10 }}>
           <Icon src={require("../../assets/advantage.png")} />
           {props?.item?.Advantage?.map((item) => {
             return <Tag title={item} style={{ marginLeft: 2 }} />;
           })}
         </Row>
-        <Row>
+        <Row style={{ marginTop: 10 }}>
           <Icon src={require("../../assets/disadvantage.png")} />
           {props?.item?.Disadvantage?.map((item) => {
             return <Tag title={item} style={{ marginLeft: 2 }} />;
