a1e2779b7d8f771f4f10a8f6cd940404f52a085f
沛妤 陳 <peifish1124@5xruby.com>
2022-11-19 00:57:11 +0800

    insert review and review_id to csv

--- backend/insert_data/review.py
+++ backend/insert_data/review.py
@@ -1,6 +1,7 @@
-from database import connect
+import pymongo
 from bson.objectid import ObjectId
 from datetime import datetime
+import pandas as pd
 
 # test
 """
@@ -9,24 +10,44 @@ print(len(list(hotel.find({}))))
 print(hotel.find({'_id': ObjectId('636c9107c2b0dc351e28abcc')}))
 """
 
-db = connect()
+# 每次跑不同飯店 只要換 csv_name & hotel_id
+
+client = pymongo.MongoClient("mongodb+srv://sylvey:54067sylvia@traviewl.t6m7fmq.mongodb.net/?retryWrites=true&w=majority")
+db = client.TraViewl
+
+csv_name = 'data/reviews/chinese_OwlStay Flip Flop Hostel Garden.csv'
+df = pd.read_csv(csv_name)
+
+review = db.Review
+# review.delete_many({})
+
+convert_time = []
+for idx, row in df.iterrows():
+    time = row['time']
+    time = time.replace('年', '/')
+    time = time.replace('月', '/')
+    time = time.split('日')[0]
+    convert_time.append(time)
+df['convert_time'] = convert_time
 
 # review
-"""
-review = db.Review
-reviewData = {
-    "hotel_id": "636d32111a537da8fd0a1bb2",
-    "language": "zh",
-    "title": "整體感覺就是五星級該有的軟硬體設備",
-    "good_text": "",
-    "bad_text": "",
-    "normal_text": "性價比高，值得推薦",
-    "time": datetime.now(),
-    "star": 9.6,
-    "importance": 0,
-    "people": "兩人同行",
-    "room_type": "雙人房"
-}
-id = review.insert_one(reviewData).inserted_id
-print(id)
-"""
+li = []
+for i in range(len(df["title"])):
+    reviewData = {
+        "hotel_id": "637397cd39de7355c1044dea",
+        "language": "zh",
+        "title": df['title'][i],
+        "good_text": "",
+        "bad_text": "",
+        "normal_text": df['normal_text'][i],
+        "time": datetime.strptime(df['convert_time'][i], '%Y/%m/%d'),
+        "star": df['star'][i],
+        "importance": 0
+    }
+    id = review.insert_one(reviewData).inserted_id
+    li.append(id)
+
+df.insert(0, "_id", li)
+df.to_csv(csv_name, index=False)
+
+print(len(list(review.find({}))))
