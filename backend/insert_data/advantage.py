64345d574670dbd490f92577678c7ad957f44436
judycpc <abc0952753111@gmail.com>
2022-11-19 19:56:43 +0800

    insert advantage & disadvantage

--- backend/insert_data/advantage.py
+++ backend/insert_data/advantage.py
@@ -4,17 +4,43 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from database import connect
 from bson.objectid import ObjectId
 from datetime import datetime
+import pandas as pd
 
 
 db = connect()
+advantage = db.Advantage
 hotel = db.Hotel
 
-# test
+def insert_advantage(path='../data/keywords_zh_with_count.xlsx'):
+    df = pd.read_excel(path)
+    df = df[['name', 'good']]
 
+    for idx, row in df.iterrows():
+        name = row['name']
+        ids = []
+        
+        for term in row['good'].split():
+            # insert
+            query = advantage.find_one({'text': term}, {'text': 0})
+            if query:
+                ids.append(str(query['_id']))
+            else:
+                _id = advantage.insert_one({'text': term}).inserted_id
+                ids.append(str(_id))
+
+        # update
+        filter = { 'name': name }
+        newvalues = { "$set": { 'advantage': ids } }
+        hotel.update_one(filter, newvalues)
+
+insert_advantage()
+
+# test
+"""
 print(list(hotel.find({})))
 print(len(list(hotel.find({}))))
 print(hotel.find({'_id': ObjectId('636c9107c2b0dc351e28abcc')}))
-
+"""
 
 
 # advantage
--- backend/insert_data/disadvantage.py
+++ backend/insert_data/disadvantage.py
@@ -1,6 +1,38 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from database import connect
 from bson.objectid import ObjectId
 from datetime import datetime
+import pandas as pd
+
+db = connect()
+disadvantage = db.Disadvantage
+hotel = db.Hotel
+
+def insert_disadvantage(path='../data/keywords_zh_with_count.xlsx'):
+    df = pd.read_excel(path)
+    df = df[['name', 'bad']]
+
+    for idx, row in df.iterrows():
+        name = row['name']
+        ids = []
+        
+        for term in row['bad'].split():
+            # insert
+            query = disadvantage.find_one({'text': term}, {'text': 0})
+            if query:
+                ids.append(str(query['_id']))
+            else:
+                _id = disadvantage.insert_one({'text': term}).inserted_id
+                ids.append(str(_id))
+
+        # update
+        filter = { 'name': name }
+        newvalues = { "$set": { 'disadvantage': ids } }
+        hotel.update_one(filter, newvalues)
+
+insert_disadvantage()
 
 # test
 """
@@ -9,7 +41,6 @@ print(len(list(hotel.find({}))))
 print(hotel.find({'_id': ObjectId('636c9107c2b0dc351e28abcc')}))
 """
 
-db = connect()
 
 # disadvantage
 """
