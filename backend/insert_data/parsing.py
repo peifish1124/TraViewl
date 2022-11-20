2522b2127fcff8d8c315a7fba9a4b9c22f2eba72
沛妤 陳 <peifish1124@5xruby.com>
2022-11-21 00:19:25 +0800

    insert fake parsing

--- backend/insert_data/parsing.py
+++ backend/insert_data/parsing.py
@@ -1,6 +1,7 @@
-from database import connect
 from bson.objectid import ObjectId
 from datetime import datetime
+import pymongo
+import pandas as pd
 
 # test
 """
@@ -9,18 +10,21 @@ print(len(list(hotel.find({}))))
 print(hotel.find({'_id': ObjectId('636c9107c2b0dc351e28abcc')}))
 """
 
-db = connect()
+client = pymongo.MongoClient("mongodb+srv://sylvey:54067sylvia@traviewl.t6m7fmq.mongodb.net/?retryWrites=true&w=majority")
+db = client.TraViewl
 
+df = pd.read_csv('data/temp_parsing.csv')
+
+parsing = db.Parsing
+parsing.delete_many({})
 
 # parsing
-"""
-parsing = db.Parsing
-parsingData = {
-    "hotel_id": "636d32111a537da8fd0a1bb2",
-    "review_id": "636d2f6ef1ef4af478870bac",
-    "aspect": "交通",
-    "opinion": "遠",
-    "sentiment": "negative"
-}
-parsing.insert_one(parsingData)
-"""
+for i in range(len(df["aspect"])):
+    parsingData = {
+        "hotel_id": "637397cd39de7355c1044df8",
+        "review_id": "",
+        "aspect": df['aspect'][i],
+        "opinion": df['opinion'][i],
+        "sentiment": df['sentiment'][i]
+    }
+    parsing.insert_one(parsingData)
