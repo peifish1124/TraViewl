23043dfb210f2e1eeccd8baa18660122cc796349
sylvey <a.few.star@gmail.com>
2022-12-09 22:17:33 +0800

    for now

--- frontend/src/toBackend/axios.tsx
+++ frontend/src/toBackend/axios.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { response } from "express";
 
 const instance = axios.create({
-  baseURL: `http://127.0.0.1:8081/`,
+  // baseURL: `http://127.0.0.1:8081/`,
+  baseURL: `http://140.112.31.158:8081/`,
   // withCredentials: true,
 });
 
